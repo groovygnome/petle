c11c8d448e44d4edb07c701944d60ac0f5d145a7
groovygnome <jrp810@proton.me>
2026-07-24 19:58:02 -0400

    add sql server, different petle answer on daily basis now

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import db from './db/dailies.js'
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/dailies/:date', async (req, res) => {
+  const result = await db.getDaily(req.params.date);
+  res.json(result);
+});
+
+app.delete('/api/dailies/delete', async (req, res) => {
+  await db.deleteDailies();
+  res.json({ ok: true });
+});
+
+app.post('/api/dailies/:date/:answer', async (req, res) => {
+  const check = await db.getDaily(req.params.date);
+  if (check.length === 0) {
+    await db.newDaily(req.params.date, req.params.answer);
+    const result = await db.getDaily(req.params.date);
+    res.json(result);
+  } else return res.json(check);
+});
+
+const PORT = 3001;
+app.listen(PORT, (error) => {
+
+  if (error) {
+    throw error;
+  }
+  console.log(`petle db, listening on port ${PORT}`);
+});
